a7ba8adccd91c6157f966fa1d8bba53f97d21a48
christoph.vogl <cvogl79@gmail.com>
2024-10-11 08:50:10 +0200

    create code for block 5

--- src/main/java/at/ac/fhcampuswien/block05/Example04.java
+++ src/main/java/at/ac/fhcampuswien/block05/Example04.java
@@ -2,6 +2,13 @@ package at.ac.fhcampuswien.block05;
 
 public class Example04 {
     // methods can have multiple parameters
+
+    /**
+     * This method prints the difference of two numbers.
+     *
+     * @param x The first number for calculating the difference.
+     * @param y The second number for calculating the difference.
+     */
     public static void printDiff(int x, int y) {
         System.out.println("The difference is " + (x - y));
     }
--- src/main/java/at/ac/fhcampuswien/block05/Exercise02.java
+++ src/main/java/at/ac/fhcampuswien/block05/Exercise02.java
@@ -3,6 +3,11 @@ package at.ac.fhcampuswien.block05;
 import java.util.Scanner;
 
 public class Exercise02 {
+    /** This method checks if a number is even.
+     *
+     * @param number Number to be tested for even.
+     * @return True if number is even.
+     */
     public static boolean isEven(int number) {
         if (number % 2 == 0) {
             return true;
