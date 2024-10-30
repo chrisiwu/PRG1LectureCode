08074b4b8098319518668a53f66b2dd5a686b6f2
christoph.vogl <cvogl79@gmail.com>
2024-10-30 13:28:10 +0100

    create code for block 11

---
+++ src/main/java/at/ac/fhcampuswien/block11/examples/Example01.java
@@ -0,0 +1,12 @@
+package at.ac.fhcampuswien.block11.examples;
+
+public class Example01 {
+    // any given object can be represented as Object type
+    String text1 = "text";
+    Object textString1 = "another string";
+    Object textString2 = text1;
+
+    // Object type to String won't work
+    Object textString3 = "another string";
+//    String text2 = textString3; // WON'T WORK!
+}
