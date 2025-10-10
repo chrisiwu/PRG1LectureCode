c1e602b99589853816a2222157b440fca715042e
Christoph Vogl <cvogl79@gmail.com>
2025-10-10 14:06:24 +0200

    Block 6

---
+++ src/main/java/at/ac/hcw/block06/examples/example01/Cat.java
@@ -0,0 +1,20 @@
+package at.ac.hcw.block06.examples.example01;
+
+/* a source code file (.java extension)
+     typically holds one class definition
+   the class represents a piece of the program
+   a class has one or more methods */
+public class Cat {
+    /* in the Cat class, the meow method
+         will hold instructions for how
+         the Cat should meow
+       methods must be declared inside a class
+         (within curly braces)
+       within the curly braces of a method,
+         write instructions for how that
+         method should be performed
+       method code is basically a set of statements */
+    public void meow() {
+
+    }
+}
